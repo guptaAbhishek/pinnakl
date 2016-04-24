5b0ed549a416ea186e8e7620818428f660168d68
Abhishek <abhishek@Abhisheks-MacBook-Pro.local>
2016-04-25 01:21:31 +0530

    desing

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,31 @@
 (function () {
     'use strict';
-    var app = angular.module('SearchApp', ['ui.router']);
+
+    var app = angular.module('SearchApp', ['ui.router'])
+    .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+        // States
+     .state("main", {
+          controller:'searchController',
+          url:"/",
+          templateUrl: "index.html"
+      })
+      .state("main.1", {
+          controller:'searchController',
+          parent: 'main',
+          url:"/1",
+          templateUrl: './views/partials/detailView.html'
+      })
+
+    }]);
+
+    app.run(['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams) {
+          $rootScope.$state = $state;
+          $rootScope.$stateParams = $stateParams;
+      }
+    ])
+
 })();
