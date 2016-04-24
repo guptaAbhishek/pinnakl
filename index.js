c40f00541d25c2bb0910bd195f3d25ae72e421d0
Abhishek <abhishek@Abhisheks-MacBook-Pro.local>
2016-04-25 01:31:01 +0530

    sfd

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.get('/',function(req,res){
 
 
 // listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(80);
+console.log("App listening on port 80");
